44bba2a6de08c420f928d0890b2b0d1b001e2851
Jaykishan <jayakishan100@gmail.com>
2017-09-12 00:18:13 +0530

    Added Insert funtion to insert rows into database

--- app/src/main/java/com/example/jaykishan/itachi/data/WeatherContract.java
+++ app/src/main/java/com/example/jaykishan/itachi/data/WeatherContract.java
@@ -1,6 +1,7 @@
 package com.example.jaykishan.itachi.data;
 
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 import android.text.format.Time;
@@ -69,6 +70,10 @@ public class WeatherContract {
         // map intent, we store the latitude and longitude as returned by openweathermap.
         public static final String COLUMN_COORD_LAT = "coord_lat";
         public static final String COLUMN_COORD_LONG = "coord_long";
+
+        public static Uri buildLocationUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 
     /* Inner class that defines the contents of the weather table */
@@ -111,6 +116,10 @@ public class WeatherContract {
 
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
+
+        public static Uri buildWeatherUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
 
         public static Uri buildWeatherLocation(String locationSetting) {
             return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
