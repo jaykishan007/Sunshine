05ae79886f1d673c7ae55b03397005904335c2c5
Jaykishan <jayakishan100@gmail.com>
2017-10-01 23:37:14 +0530

    replaced sharedpreferences with utility code

--- app/src/main/java/com/example/jaykishan/itachi/MainActivity.java
+++ app/src/main/java/com/example/jaykishan/itachi/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.jaykishan.itachi;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -69,10 +67,7 @@ public class MainActivity extends AppCompatActivity {
     private void openPreferredLocationInMap()
     {
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-
-        String location=prefs.getString(getString(R.string.pref_location_key),getString(R.string.pref_location_default));
-
+        String location = Utility.getPreferredLocation(this);
 
         Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
                 .appendQueryParameter("q",location)
