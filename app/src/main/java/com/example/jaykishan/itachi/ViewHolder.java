1f727e739043fa3625e041105ddf6dc7d50adfce
Jaykishan <jayakishan100@gmail.com>
2017-10-04 23:12:48 +0530

    Created ViewHolder pattern to remove unnecessary ID calls

---
+++ app/src/main/java/com/example/jaykishan/itachi/ViewHolder.java
@@ -0,0 +1,27 @@
+package com.example.jaykishan.itachi;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by jaykishan on 04/10/17.
+ */
+
+public class ViewHolder {
+
+    public final ImageView iconView;
+    public final TextView dateView;
+    public final TextView descriptionView;
+    public final TextView highTempView;
+    public final TextView lowTempView;
+
+    public ViewHolder(View view) {
+        iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+        dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
+        descriptionView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+        highTempView = (TextView) view.findViewById(R.id.list_item_high_textview);
+        lowTempView = (TextView) view.findViewById(R.id.list_item_low_textview);
+    }
+
+}
