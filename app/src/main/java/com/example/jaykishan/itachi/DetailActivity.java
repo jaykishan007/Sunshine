3f1178efcaaf813803145f1f010e5b42bf59ff28
Jaykishan <jayakishan100@gmail.com>
2017-10-22 03:16:49 +0530

    Toolbar replaced with action bar by remving the style as noAction and custom styles to MainActivity to show logo in action bar

--- app/src/main/java/com/example/jaykishan/itachi/DetailActivity.java
+++ app/src/main/java/com/example/jaykishan/itachi/DetailActivity.java
@@ -3,7 +3,6 @@ package com.example.jaykishan.itachi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -37,8 +36,6 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
--- app/src/main/java/com/example/jaykishan/itachi/MainActivity.java
+++ app/src/main/java/com/example/jaykishan/itachi/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -35,14 +34,12 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
         }
         else {
             mTwoPane = false;
+            getSupportActionBar().setElevation(0f);
         }
 
         ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 
