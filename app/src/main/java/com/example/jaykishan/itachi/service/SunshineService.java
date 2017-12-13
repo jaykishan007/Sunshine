40e0258fbba10cfe3eb1530768b21aee62c2750f
Jaykishan <jayakishan100@gmail.com>
2017-12-14 00:07:15 +0530

    Alarm manager to trigger the service through the BroadCast receiver using pending intent

--- app/src/main/java/com/example/jaykishan/itachi/service/SunshineService.java
+++ app/src/main/java/com/example/jaykishan/itachi/service/SunshineService.java
@@ -1,6 +1,7 @@
 package com.example.jaykishan.itachi.service;
 
 import android.app.IntentService;
+import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
@@ -328,6 +329,19 @@ public class SunshineService extends IntentService {
         locationCursor.close();
         // Wait, that worked?  Yes!
         return locationId;
+
+    }
+
+
+    public static class AlarmReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent sendIntent = new Intent(context, SunshineService.class);
+            sendIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, intent.getStringExtra(SunshineService.LOCATION_QUERY_EXTRA));
+            context.startService(sendIntent);
+
+        }
     }
 
 }
