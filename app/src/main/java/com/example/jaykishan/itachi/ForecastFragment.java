96febc9716a5115e1dbd4d307d0924565acd5afd
Jaykishan <jayakishan100@gmail.com>
2017-12-13 22:59:24 +0530

    Removed FetchWeatherTask Async task and replaced the update weather funtionality with the Intent service we created

--- app/src/main/java/com/example/jaykishan/itachi/ForecastFragment.java
+++ app/src/main/java/com/example/jaykishan/itachi/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.jaykishan.itachi;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.jaykishan.itachi.data.WeatherContract;
+import com.example.jaykishan.itachi.service.SunshineService;
 
 
 /**
@@ -154,9 +156,10 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     private void updateWeather() {
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+        Intent intent = new Intent(getActivity(), SunshineService.class);
+        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,
+                Utility.getPreferredLocation(getActivity()));
+        getActivity().startService(intent);
     }
 
     @Override
