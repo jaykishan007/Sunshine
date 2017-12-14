c30e9473aced8b41cef0e116b98fcefe995e0faa
Jaykishan <jayakishan100@gmail.com>
2017-12-14 22:50:52 +0530

    SunshineAuthenticatorService had been added : It allows the account manager to access the empty authenticator we have created

---
+++ app/src/main/java/com/example/jaykishan/itachi/sync/SunshineAuthenticatorService.java
@@ -0,0 +1,29 @@
+package com.example.jaykishan.itachi.sync;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by jaykishan on 14/12/17.
+ */
+
+public class SunshineAuthenticatorService extends Service {
+    // Instance field that stores the authenticator object
+    private SunshineAuthenticator mAuthenticator;
+
+    @Override
+    public void onCreate() {
+        // Create a new authenticator object
+        mAuthenticator = new SunshineAuthenticator(this);
+    }
+
+    /*
+     * When the system binds to this Service to make the RPC call
+     * return the authenticator's IBinder.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mAuthenticator.getIBinder();
+    }
+}
